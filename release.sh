f2157f9b7423d03979a7ac73403f54f876af3358
r32 <56782055@qq.com>
2023-07-07 00:48:41 +0800

    Updated haxelib

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 #
 rm -rf release
 mkdir -p release
-cp -R -u src haxelib.json run.n yuicompressor-2.4.8.jar release
+cp -R -u src haxelib.json yuicompressor-2.4.8.jar release
 chmod -R 777 release
 cd release
-zip -r release.zip ./ -x 'src/minify*' && mv release.zip ../
+zip -r release.zip ./ && mv release.zip ../
 cd ..
